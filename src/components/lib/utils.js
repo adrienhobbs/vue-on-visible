be68b8850bf9bb61e5125b9faaa03356f91231fc
Adrien Hobbs <adrien.hobbs@gmail.com>
2018-06-25 14:59:22 -0400

    allow threshold to be used for triggering visible events

--- src/components/lib/utils.js
+++ src/components/lib/utils.js
@@ -1,5 +1,5 @@
-export const getViewportHeight = function getVpHeight() {
-  return window.innerHeight || document.documentElement.clientHeight
-}
+export const getViewportHeight = () =>
+  window.innerHeight || document.documentElement.clientHeight
+
 export const isAbove = ({ bottom, offset }) => bottom < 0 - offset
 export const isBelow = ({ top, offset }) => top > getViewportHeight() + offset
