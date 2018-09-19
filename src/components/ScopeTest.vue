304264ef6027d60a04a78c0d38aca839725b7c6a
Adrien Hobbs <adrien.hobbs@gmail.com>
2018-09-19 16:10:59 -0400

    formatting

--- src/components/ScopeTest.vue
+++ src/components/ScopeTest.vue
@@ -1,7 +1,11 @@
 <template>
   <div style="position: relative; width: 100%; height: 100%">
-    <div style="top: 10px; position: absolute;">{{viewport}}</div>
-    <div style="bottom: 10px; position: absolute;">{{viewport}}</div>
+    <div style="top: 10px; position: absolute;">
+      {{viewport}}
+    </div>
+    <div style="bottom: 10px; position: absolute;">
+      {{viewport}}
+    </div>
   </div>
 </template>
 
