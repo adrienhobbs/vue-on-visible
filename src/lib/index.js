f9f96e3a5d0b7432b34d721ad8954d5783a61e2a
Adrien Hobbs <adrien.hobbs@gmail.com>
2018-09-19 15:15:37 -0400

    remove unnecessary imports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,3 @@
+import './polyfills'
 export { default } from './plugin'
-export { default as OnVisibleMixin } from './mixin'
-export { default as OnVisibleEmitter } from '../components/OnVisibleEmitter'
 export { default as OnVisible } from '../components/OnVisible'
