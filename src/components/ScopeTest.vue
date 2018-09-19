b2c501f1b3af6e6a9c02b1d93766691825722161
Adrien Hobbs <adrien.hobbs@gmail.com>
2018-09-19 15:43:05 -0400

    Add some testing components

---
+++ src/components/ScopeTest.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="position: relative; width: 100%; height: 100%">
+    <div style="top: 10px; position: absolute;">{{viewport}}</div>
+    <div style="bottom: 10px; position: absolute;">{{viewport}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScopeTest',
+  props: ['viewport'],
+}
+</script>
+
